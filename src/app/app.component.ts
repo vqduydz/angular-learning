dbf455b20e705d9252e0bf5018215e8dfd7267be
vqduydz <vqduydz@gmail.com>
2024-04-06 16:42:51 +0700

    #1. ngModel - Data/Events/Two-way binding.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-learning';
+
+  name: string | undefined;
+  resetName() {
+    this.name = undefined;
+  }
+  ngOnInit() {}
 }
